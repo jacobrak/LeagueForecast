4df147d75c0f8d0f2fe34d8b422febed0443ff21
Jacob Rak <feliks.jacob@gmail.com>
2024-08-11 15:54:20 +0200

    New file for shared functions

---
+++ Scraping/functions.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import pandas as pd
+def save_dataframe_to_csv(df: pd.DataFrame, filename: str) -> None:
+    output_path = Path("data") / filename
+
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    df.to_csv(output_path, index=False)
+    print(f"DataFrame saved to {output_path}")
